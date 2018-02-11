18279bd983fc85688eac3bf56cee157e2f1b7113
Benzoh.Wada <benzoh.wada@gmail.com>
2018-02-11 17:11:34 +0900

    - Yahooニュースの表示する - index.html.slimを修正 - Net::HTTPで外部サイトにアクセス - data.js.erbを修正

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -5,12 +5,21 @@ class AjaxController < ApplicationController
   end
 
   def data
-    # @data = User.where 'name like ?', '%' + params[:name] + '%'
-    @data = User.where(name: params[:name]).or(User.where(id: params[:id]))
-    # @data = User.where(id: params[:id])
-    @data = @data.to_json.html_safe
-
-    # @ajax_data = User.find 1
+    # Net::HTTPで外部サイトにアクセス
+    url = URI('https://www6.nhk.or.jp/special/feed/schedule.html')
+    # url = URI('https://news.yahoo.co.jp/pickup/rss.xml')
+    # url = URI('https://www.start30blog.com/feed/')
+    http = Net::HTTP.new(url.host)
+    response = http.get(url)
+    @data = Hash.from_xml(response.body).to_json.html_safe
     # raise
+    
+    # # @data = User.where 'name like ?', '%' + params[:name] + '%'
+    # @data = User.where(name: params[:name]).or(User.where(id: params[:id]))
+    # # @data = User.where(id: params[:id])
+    # @data = @data.to_json.html_safe
+
+    # # @ajax_data = User.find 1
+    # # raise
   end
 end
