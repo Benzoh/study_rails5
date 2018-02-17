6f083ddb117fd42b56ee43541c0d99c1ab2aa100
Benzoh.Wada <benzoh.wada@gmail.com>
2018-02-17 10:53:31 +0900

    - モデルの修正 - コントローラーとびゅーを修正 -- indexの処理 -- updateの処理

--- app/models/board_message.rb
+++ app/models/board_message.rb
@@ -1,2 +1,5 @@
 class BoardMessage < ApplicationRecord
+    belongs_to :board_user
+
+    validate :content, presence: {message: 'は、必須です'}
 end
--- app/models/board_user.rb
+++ app/models/board_user.rb
@@ -1,2 +1,6 @@
 class BoardUser < ApplicationRecord
+    belongs_to :account
+    has_many :board_message
+
+    validate :nickname, presence: {message: 'は、必須です'}
 end
