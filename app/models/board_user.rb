4dae7214dda201b0f0f5e06d6956e7c7941a9732
Benzoh.Wada <benzoh.wada@gmail.com>
2018-02-17 11:26:10 +0900

    - BoardUsersControllerのビュー

--- app/models/board_user.rb
+++ app/models/board_user.rb
@@ -2,5 +2,5 @@ class BoardUser < ApplicationRecord
     belongs_to :account
     has_many :board_message
 
-    validate :nickname, presence: {message: 'は、必須です'}
+    validates :nickname, presence: {message: 'は、必須です'}
 end
