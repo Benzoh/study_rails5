9cd21e3d0d7592e10e38caa30ab40db9d10321c4
Benzoh.Wada <benzoh.wada@gmail.com>
2018-02-17 12:01:21 +0900

    - BoarMessagesControllerクラス

--- app/models/board_message.rb
+++ app/models/board_message.rb
@@ -1,5 +1,5 @@
 class BoardMessage < ApplicationRecord
     belongs_to :board_user
 
-    validate :content, presence: {message: 'は、必須です'}
+    validates :content, presence: {message: 'は、必須です'}
 end
